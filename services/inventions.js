7b5f86ee9596b4fd8220ba5bcdb551029b4f8fe2
thesirix <renaud.d.mercier@gmail.com>
2024-02-05 15:18:11 +0100

    ça marchhhe

--- services/inventions.js
+++ services/inventions.js
@@ -63,11 +63,36 @@ function displayAllInventions() {
     It then displays the details of the invention in the target element from the DOM.
     TODO: Implement this function.
 */
-function displayInvention() {
-    // Get the id of the invention from the URL
-    const urlParams = new URLSearchParams(window.location.search);
-    const id = urlParams.get('id');
-    // Get the target element from the DOM
-    // Get the invention object from the array "inventions" using the id
-    // Display the details of the invention in the target element
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function displayInvention() {     // Get the id of the invention from the URL  
+       const urlParams = new URLSearchParams(window.location.search);     const id = urlParams.get('id');    
+        // Get the target element from the DOM    
+         const htmlTitleTarget = document.getElementById("invention_title")   
+           const htmlTarget = document.getElementById("invention_details");    
+            // Get the invention object from the array "inventions" using the id   
+              let item = inventions.find(item => item.id == id);     console.log(item)   
+                // Display the details of the invention in the target element  
+                   htmlTitleTarget.innerHTML = item.name    
+                    htmlTarget.innerHTML +=  `
+
+                        <img src="/assets/images/inventions/${item.image}" alt="${item.name}">      
+                        <p>${item.description}</p>    
+                        <p>Inventé par ${item.inventor} en ${item.year}</p>      
+                     `     
+                                 }
